6a6160e26520f06e39bf10728d2a46cdb76c7ffb
maitzeth <andre801mz@gmail.com>
2024-06-17 22:56:08 -0300

    feat: :sparkles: adding searcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { PhraseForm } from '@/components/PhraseForm';
-import { Container } from '@/components/ui';
-import { Phrase } from '@/types/phrase';
-import { useState } from 'react';
 import { PhraseRenderer } from '@/components/PhraseRenderer';
+import { Container, Input } from '@/components/ui';
+import { Phrase } from '@/types/phrase';
+import { debounce } from 'lodash';
+import { useMemo, useState } from 'react';
 
 
 function App() {
@@ -11,16 +12,23 @@ function App() {
     return savedPhrases ? JSON.parse(savedPhrases) : [];
   });
 
+  const [searchValue, setSearchValue] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  /**
+   * Handle creation for a new phrase to be added
+   */
   const handleCreateNewPhrase = (phrase: Phrase) => {
     setPhrases((prev) => {
       const newState = [...prev, phrase];
-
       localStorage.setItem('phrases', JSON.stringify(newState));
-
       return newState;
     });
   };
 
+  /**
+   * Handle delete for phrases
+   */
   const handleDeletePhrase = (id: string) => {
     setPhrases((prev) => {
       const newState = prev.filter((phrase) => phrase.id !== id);
@@ -29,10 +37,40 @@ function App() {
     });
   };
 
+  /**
+   * Debounce function from lodash to limit the rate of updates to the search term.
+   */
+  const debouncedSetSearchTerm = useMemo(
+    () => debounce((term: string) => setSearchTerm(term), 300),
+    []
+  );
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+    debouncedSetSearchTerm(event.target.value);
+  };
+
+  /**
+   * Filter phrases based on the search term.
+   */
+  const filteredPhrases = useMemo(
+    () => phrases.filter((phrase) =>
+      phrase.phrase.toLowerCase().includes(searchTerm.toLowerCase())
+    ),
+    [phrases, searchTerm]
+  );
+
   return (
     <Container as="main">
+      <Input
+        id="search"
+        value={searchValue}
+        type="text"
+        placeholder="Buscar frase"
+        onChange={handleSearchChange}
+      />
       <PhraseForm handleCreateNewPhrase={handleCreateNewPhrase} />
-      <PhraseRenderer phrases={phrases} deletePhrase={handleDeletePhrase} />
+      <PhraseRenderer phrases={filteredPhrases} deletePhrase={handleDeletePhrase} />
     </Container>
   );
 }
